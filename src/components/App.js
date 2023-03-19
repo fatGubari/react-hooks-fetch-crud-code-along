ca6383a9e36979ae4a079c05082ab4c7f717f89f
fatGubari <fatimajubary@gmail.com>
2023-03-19 17:41:41 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
